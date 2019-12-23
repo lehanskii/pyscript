120724c7bbf6a35a83b8ad55817553378d232cae
ponteley <myEmail@example.com>
2019-12-23 19:33:40 +0300

    New version of program

--- main.py
+++ main.py
@@ -4,76 +4,100 @@ from bs4 import BeautifulSoup as bs
 import time
 
 url = "https://storiesig.com/stories/"
+dir = "Enter your dir with file persons.txt"
 
-def pars_target(target):
-    target_dir = 'D:\\Новая папка\\Data\\pars\\{0}'.format(target)
-    for_check = "---"
-    try:
-        connect = requests.get(url + target, headers={"accept": "*/*",
-                                                      "user-agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0"})
-        soup = bs(connect.content, "html.parser")
-        images = soup.find_all("img")
-        videos = soup.find_all("video")
-        if len(images) == 0:
-            time.sleep(20)
+
+class StoriesParser:
+    def __init__(self, target):
+        self.target = target
+        self.target_dir = dir.format(target)
+        self.image_stories = []
+        self.video_stories = []
+        self.operation_success = False
+        self.find_error = False
+
+    def get_stories(self):
+        try:
+            connect = requests.get(url + self.target)
+            soup = bs(connect.content, "html.parser")
+            for image_story in soup.find_all("img"):
+                self.image_stories.append(image_story['src'])
+            for video_story in soup.find_all("video"):
+                self.video_stories.append(video_story['src'])
+        except:
+            print("Error in get_stories method: " + self.target)
+            self.find_error = True
+
+    def save_stories(self):
+        try:
+            if (len(self.image_stories) == 0) and (len(self.video_stories) == 0):
+                time.sleep(10)
+                return
+            number_of_stories = len([f for f in os.listdir(self.target_dir)])
+            for image_story in self.image_stories:
+                time.sleep(0.5)
+                connect = requests.get(image_story)
+                image = connect.content
+                if self.duplicate_checker(image):
+                    number_of_stories += 1
+                    self.operation_success = True
+                    with open(self.target_dir + "\\{0}.jpg".format(number_of_stories), "wb") as file:
+                        file.write(image)
+            for video_story in self.video_stories:
+                time.sleep(0.5)
+                connect = requests.get(video_story, stream=True)
+                video = connect.content
+                if self.duplicate_checker(video):
+                    with open(self.target_dir + "\\{0}.mp4".format(number_of_stories), "wb") as file:
+                        for chunk in connect.iter_content(chunk_size=1024):
+                            if chunk:
+                                file.write(chunk)
+        except:
+            print("Error in save_stories method:" + self.target)
+            self.find_error = True
+
+    def duplicate_checker(self, image):
+        try:
+            objects = [f for f in os.listdir(self.target_dir)]
+            for object in objects:
+                with open(self.target_dir + "\\{0}".format(object), "rb") as binary_file:
+                    if image == binary_file.read():
+                        return False
+            return True
+        except:
+            print("Error in duplicate_checker method:" + self.target)
+            self.find_error = True
+
+    def print_report(self):
+        if self.find_error:
             return
-        if not os.path.exists(target_dir):
-            os.makedirs(target_dir)
-        for_count = len([f for f in os.listdir(target_dir)])
-        for image in images:
-            time.sleep(0.2)
-            with requests.get(image['src']) as connect:
-                binaryimage = connect.content
-            if (check_list(target_dir, binaryimage)):
-                for_count += 1
-                for_check = "+++"
-                with open(target_dir + "\\{0}.jpg".format(for_count), "wb") as file:
-                    file.write(binaryimage)
-        #    for video in videos:
-        #        time.sleep(0.2)
-        #        with requests.get(video['src']) as connect:
-        #            binaryvideo = connect.content
-        #        if (check_list(target_dir, binaryvideo)):
-        #            for_count += 1
-        #            for_check = "+++"
-        #            with open(target_dir + "\\{0}.mp4".format(for_count), "wb") as file:
-        #                for chunk in binaryvideo:
-        #                    file.write(chunk)
+        if self.operation_success:
+            print("Target: " + self.target + " was successful processed with +++")
+        else:
+            print("Target: " + self.target + " was successful processed with ---")
 
-        print_report(target, True, for_check)
-    except:
-        print_report(target, False)
-
-    time.sleep(20)
+    def run(self):
+        self.get_stories()
+        self.save_stories()
+        self.print_report()
 
 
-def check_list(targets_path, binaryobject):
-    objects = [f for f in listdir(targets_path)]
-    for object in objects:
-        with open(targets_path + "\\{0}".format(object), "rb") as binaryfile:
-            if binaryfile.read() == binaryobject:
-                return False
-    return True
+class Main:
+    def __init__(self):
+        self.list_for_targets = []
 
+    def get_targets_list(self):
+        with open(dir.format("persons.txt")) as file:
+            for target in file:
+                self.list_for_targets.append(target)
 
-def targets_list(targets_path):
-    list_of_target = []
-    with open(targets_path) as file:
-        for target in file:
-            list_of_target.append(target)
-    return list_of_target
+    def run(self):
+        print("Targets processing started.")
+        self.get_targets_list()
+        for target in self.list_for_targets:
+            StoriesParser(target.rstrip()).run()
+            time.sleep(10)
+        print("Targets processing completed")
+        a = int(input())
 
-def print_report(target, well, *for_check):
-    if well:
-        print("Target: " + target + " was successful processed with " + for_check[0])
-    else:
-        print("Error: " + target)
-
-
-targets_list_path = "D:\\Новая папка\\Data\\pars\\persons.txt"
-print("Targets processing started.")
-for target in targets_list(targets_list_path):
-    pars_target(target.rstrip())
-
-print("Targets processing completed")
-a = input()
+Main().run()
